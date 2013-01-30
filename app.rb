884896692e5967c5cf4e532ab8aad44b489c8644
Chris Allnutt <chris@chrisallnutt.com>
2013-01-30 09:43:41 -0500

    remove debugging line

--- app.rb
+++ app.rb
@@ -15,13 +15,13 @@ get '/*' do
   @cache = @client.cache("wu_api_proxy")
 
   cache_response = @cache.get(cache_key)
-  
+
   if cache_response != nil
     return cache_response.value
   end
 
   response = open(ENV['API_HOST']+ENV['API_BASE_URI']+"/"+ENV['API_KEY']+"/"+api_request_path).read
-  puts api_request_path
+
   @cache.put(cache_key, response, :expires_in=>600)
 
   response
